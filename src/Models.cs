e9579c7e4cb81fcd1a9a9949f41bf6a926b31d65
Rafael Miranda <rafaelpadovezi@gmail.com>
2025-07-28 00:34:48 +0100

    feat: add o11y

--- src/Models.cs
+++ src/Models.cs
@@ -4,7 +4,11 @@ public record PaymentRequest(Guid CorrelationId, decimal Amount);
 
 public record PaymentSummaryResponse(Summary Default, Summary Fallback);
 
-public struct Summary(int TotalRequests, decimal TotalAmount);
+public struct Summary(int totalRequests, decimal totalAmount)
+{
+    public int TotalRequests { get; set; } = totalRequests;
+    public decimal TotalAmount { get; set; } = totalAmount;
+}
 
 public record PaymentApiRequest(Guid CorrelationId, decimal Amount, DateTimeOffset RequestedAt);
 
